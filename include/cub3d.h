0886b410a9b4efe867bc08caaad5e0976c1b7875
Eguelin <eguelin@student.42lyon.fr>
2023-10-10 18:40:49 +0200

    restart

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acarlott <acarlott@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 13:43:10 by eguelin           #+#    #+#             */
-/*   Updated: 2023/10/10 14:25:35 by acarlott         ###   ########lyon.fr   */
+/*   Updated: 2023/10/10 18:36:18 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,5 @@
 # define CUB3D_H
 # include "mylib.h"
 # include "mlx.h"
-
-# define TRUE 0
-# define FALSE -1
-
-typedef enum e_error
-{
-	SUCCESS;
-	TRUE;
-	FALSE;
-	EMPTY_ENV,
-	ERROR_PARAM,
-}	t_error;
-
-typedef struct e_cube 
-{
-	char	*NO_texture;
-	char	*SO_texture;
-	char	*WE_texture;
-	char	*EA_texture;
-	char	*F_colors;
-	char	*c_colors;
-}	t_cube;
-
-int	parsing(int argc, char **argv);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,36 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acarlott <acarlott@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:30:44 by eguelin           #+#    #+#             */
-/*   Updated: 2023/10/10 14:13:53 by acarlott         ###   ########lyon.fr   */
+/*   Updated: 2023/10/10 18:39:10 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static int	check_init(int argc, char **argv, char **env)
-{
-	if (!env)
-		return (EMPTY_ENV, ft_printf_fd(STDERR_FILENO, "empty env !\n"));
-	if (argv != 1) 
-	{
-		ft_printf_fd(STDERR_FILENO, "wrong number of parameters, 1 expected\n");
-		return (ERROR_PARAM);
-	}
-	return (SUCCESS);
-}
-
 int	main(int argc, char **argv, char **env)
 {
-	int	i;
-	
-	i = check_init(argc, argv, env);
-	if (i != SUCCESS)
-		return (i);
-	i = parsing(argc, argv);
-	if (i != SUCCESS)
-		return (i);
-	return (SUCCESS);
+	(void)argc;
+	(void)argv;
+	(void)env;
+	return (0);
 }
