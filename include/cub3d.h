23640cda8573ec72ce686b95cf6e9f8cbefd798f
Eguelin <eguelin@student.42lyon.fr>
2023-10-10 19:16:59 +0200

    up date cub3d.h

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 13:43:10 by eguelin           #+#    #+#             */
-/*   Updated: 2023/10/10 18:48:05 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/10/10 19:16:26 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,8 @@ typedef enum e_error
 	MALLOC_ERROR
 	ENV_ERROR
 }	t_error;
+
+/////// [utils] ///////
+int	ft_perror(const char *s, int error);
 
 #endif
