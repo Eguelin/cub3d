187f9f1ebff3769ace0e62acc2442da85838cea0
acarlott <nagabtc@gmail.com>
2023-10-18 11:39:24 +0200

    Rework ft_destroy_texture ok

--- src/utils/ft_exit.c
+++ src/utils/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: acarlott <acarlott@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 18:47:15 by eguelin           #+#    #+#             */
-/*   Updated: 2023/10/18 11:28:58 by acarlott         ###   ########lyon.fr   */
+/*   Updated: 2023/10/18 11:38:48 by acarlott         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,15 @@ void	ft_exit(t_cub3d *cub, char const *s, int return_value)
 
 void	ft_destroy_texture(t_cub3d *cub)
 {
-	if (cub->texture[0].img)
-		mlx_destroy_image(cub->mlx, cub->texture[0].img);
-	if (cub->texture[1].img)
-		mlx_destroy_image(cub->mlx, cub->texture[1].img);
-	if (cub->texture[2].img)
-		mlx_destroy_image(cub->mlx, cub->texture[2].img);
-	if (cub->texture[3].img)
-		mlx_destroy_image(cub->mlx, cub->texture[3].img);
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (cub->texture[i].img)
+			mlx_destroy_image(cub->mlx, cub->texture[i].img);
+		i++;
+	}
 }
 
 static void	ft_free_cub(t_cub3d *cub)
